bf872e951c3c7d3248b38242812f1ae113d0dcbd
hq <hq112415@gmail.com>
2019-10-12 17:00:28 +0800

    测试tx

--- src/test/java/org/mybatis/my/tx/SqlSessionTemplateTest1.java
+++ src/test/java/org/mybatis/my/tx/SqlSessionTemplateTest1.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 
 public class SqlSessionTemplateTest1 {
+
     @Test
     public void testCase() throws Exception {
         ApplicationContext context = new ClassPathXmlApplicationContext("org/mybatis/my/tx/application-context.xml");
@@ -18,6 +23,25 @@ public class SqlSessionTemplateTest1 {
 
     @Test
     public void testTx() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("org/mybatis/my/tx/application-context.xml");
+        SqlSessionTemplate template = (SqlSessionTemplate) context.getBean("readSqlSession");
+        StudentMapper mapper = template.getMapper(StudentMapper.class);
+
+        DataSourceTransactionManager txManager = (DataSourceTransactionManager) context.getBean("txManager");
+        TransactionDefinition td = new DefaultTransactionDefinition();
+        TransactionStatus txStatus = txManager.getTransaction(td);
+        try {
+            mapper.insert(new Student().setName("小明").setNum(2L));
+            System.out.println(mapper.getAllStudent());
+            //回滚
+            int i = 1 / 0;
+            txManager.commit(txStatus);
+        } catch (Exception e) {
+            System.out.println(mapper.getAllStudent());
+            System.out.println(e);
+            txManager.rollback(txStatus);
+            System.out.println(mapper.getAllStudent());
+        }
 
     }
 }
--- src/test/java/org/mybatis/my/tx/StudentMapper.java
+++ src/test/java/org/mybatis/my/tx/StudentMapper.java
@@ -1,9 +1,16 @@
 package org.mybatis.my.tx;
 
 
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 public interface StudentMapper {
 
     Student selectById(Long id);
 
     void insert(Student stu);
+
+    @Select("select * from student")
+    List<Student> getAllStudent();
 }
